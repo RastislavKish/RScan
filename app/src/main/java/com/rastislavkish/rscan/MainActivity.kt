3bd1c5281ca5f41ff8281f43550e1165e816a736
RastislavKish <rastislav.kish@protonmail.com>
2021-06-01 05:49:50 +0200

    Initial code commit
    This is the initial code commit. As of now, the application implements the [Code scanner library](https://github.com/yuriy-budiyev/code-scanner) as well as the [RTK library](https://github.com/RastislavKish/RtkKotlinAndroid)
    It should be capable of requesting camera permission, scanning for barcodes and beeping when one is found, announcing its value.

---
+++ app/src/main/java/com/rastislavkish/rscan/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.rastislavkish.rscan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.rastislavkish.rtk.Sound
+import com.rastislavkish.rtk.Speech
+
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.ScanMode
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+
+import com.google.zxing.Result
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var speech: Speech
+
+    private val barcodeScannerBeep=Sound()
+    private lateinit var codeScanner: CodeScanner
+    private lateinit var permissionsRequester: PermissionsRequester
+
+    private var lastDetectedBarcode=""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        barcodeScannerBeep.load("BarcodeScannerBeep.opus", this)
+
+        permissionsRequester=PermissionsRequester(this)
+        if (!permissionsRequester.permissionsGranted) {
+            permissionsRequester.requestPermissions(this)
+            }
+
+        speech=Speech(this)
+
+        val scannerView=findViewById<CodeScannerView>(R.id.codeScannerView)
+        codeScanner=CodeScanner(this, scannerView)
+        codeScanner.autoFocusMode=AutoFocusMode.CONTINUOUS
+        codeScanner.scanMode=ScanMode.CONTINUOUS
+        codeScanner.decodeCallback=DecodeCallback(this::decodeCallback)
+        }
+    override fun onPause()
+        {
+        super.onPause()
+
+        codeScanner.releaseResources()
+        }
+    override fun onResume()
+        {
+        super.onResume()
+
+        codeScanner.startPreview()
+        }
+
+    fun decodeCallback(result: Result)
+        {
+        if (result.text!=lastDetectedBarcode) {
+            lastDetectedBarcode=result.text
+
+            barcodeScannerBeep.play()
+            speech.speak(result.text)
+
+            }
+        }
+
+    }
---
+++ app/src/main/java/com/rastislavkish/rscan/PermissionsRequester.kt
@@ -0,0 +1,27 @@
+package com.rastislavkish.rscan
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class PermissionsRequester(context: Context) {
+
+    val cameraPermission: Boolean
+    get() = ContextCompat.checkSelfPermission(context, "android.permission.CAMERA")==PackageManager.PERMISSION_GRANTED
+
+    val permissionsGranted: Boolean
+    get() = cameraPermission
+
+    private val context=context
+
+    fun requestPermissions(activity: Activity)
+        {
+        ActivityCompat.requestPermissions(activity, arrayOf("android.permission.CAMERA"), 1)
+        }
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE=1
+        }
+    }
