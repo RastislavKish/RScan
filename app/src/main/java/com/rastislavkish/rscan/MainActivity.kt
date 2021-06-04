b509c7a474b2509f92ceb17812acde8ca139c2a4
RastislavKish <rastislav.kish@protonmail.com>
2021-06-05 01:35:52 +0200

    Remove the code-scanner library
    Removes all declarations and usages of the code-scanner library. MLKit is now the only recognizer for barcodes.

--- app/src/main/java/com/rastislavkish/rscan/MainActivity.kt
+++ app/src/main/java/com/rastislavkish/rscan/MainActivity.kt
@@ -6,21 +6,12 @@ import android.os.Bundle
 import com.rastislavkish.rtk.Sound
 import com.rastislavkish.rtk.Speech
 
-import com.budiyev.android.codescanner.CodeScanner
-import com.budiyev.android.codescanner.AutoFocusMode
-import com.budiyev.android.codescanner.ScanMode
-import com.budiyev.android.codescanner.CodeScannerView
-import com.budiyev.android.codescanner.DecodeCallback
-
-import com.google.zxing.Result
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var speech: Speech
 
     private val barcodeScannerBeep=Sound()
     private lateinit var scanner: BScanner
-    private lateinit var codeScanner: CodeScanner
     private lateinit var permissionsRequester: PermissionsRequester
 
     private var lastDetectedBarcode=""
@@ -38,37 +29,8 @@ class MainActivity : AppCompatActivity() {
 
         speech=Speech(this)
 
-        val scannerView=findViewById<CodeScannerView>(R.id.codeScannerView)
-        codeScanner=CodeScanner(this, scannerView)
-        codeScanner.autoFocusMode=AutoFocusMode.CONTINUOUS
-        //codeScanner.scanMode=ScanMode.CONTINUOUS
-        //codeScanner.decodeCallback=DecodeCallback(this::decodeCallback)
-
         scanner=BScanner(this)
         scanner.addBarcodeDetectedListener(this::barcodeDetected)
-        }
-    override fun onPause()
-        {
-        super.onPause()
-
-        //codeScanner.releaseResources()
-        }
-    override fun onResume()
-        {
-        super.onResume()
-
-        //codeScanner.startPreview()
-        }
-
-    fun decodeCallback(result: Result)
-        {
-        if (result.text!=lastDetectedBarcode) {
-            lastDetectedBarcode=result.text
-
-            barcodeScannerBeep.play()
-            speech.speak(result.text)
-
-            }
         }
 
     fun barcodeDetected(value: String)
